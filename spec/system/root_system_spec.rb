f5fdde619a746f1ddfa8ef6e272ab698e5f165ae
Anton-Ivanov <anton.i@hey.com>
2024-05-29 20:55:40 +0300

    improve system test

--- spec/system/root_system_spec.rb
+++ spec/system/root_system_spec.rb
@@ -5,10 +5,10 @@ require 'rails_helper'
 RSpec.describe 'Root path' do
   before { driven_by(:rack_test) }
 
-  context 'when submit empty form' do
+  context 'when visit "/" path' do
     before { visit '/' }
 
-    it 'should render error flash message' do
+    it 'should render home controller' do
       expect(page).to have_selector 'h1', exact_text: 'Home#index'
       expect(page).to have_current_path root_path
     end
