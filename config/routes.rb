e57d89534a46ae57f09b89d574aa461fd7f5d185
Anton-Ivanov <anton.i@hey.com>
2024-05-29 20:25:03 +0300

    add home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -9,7 +10,5 @@ Rails.application.routes.draw do
   get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
-  devise_scope :user do
-    root 'devise/sessions#new'
-  end
+  root 'home#index'
 end
