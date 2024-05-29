ee2cfbe3218d9d6b20e2297ce42466aa7866e406
Anton-Ivanov <anton.i@hey.com>
2024-05-29 13:59:30 +0300

    make rubocop happy and add config file named ".rybocop.yml"

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,12 +9,10 @@ require 'rspec/rails'
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
-  abort e.to_s.strip
+  abort(e.to_s.strip)
 end
 RSpec.configure do |config|
-  config.fixture_paths = [
-    Rails.root.join('spec/fixtures')
-  ]
+  config.fixture_paths = [Rails.root.join('spec/fixtures')]
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
