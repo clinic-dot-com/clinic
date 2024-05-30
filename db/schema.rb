1aa739b1ddc68a5835b7d2db0ee95fc7253883fa
Anton-Ivanov <anton.i@hey.com>
2024-05-30 14:13:39 +0300

    introduce the DoctorCategory model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_29_142125) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_30_105949) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "appointments", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "doctor_id", null: false
+    t.string "status"
+    t.text "recommendation"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["doctor_id"], name: "index_appointments_on_doctor_id"
+    t.index ["user_id"], name: "index_appointments_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "doctor_categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_doctor_categories_on_name", unique: true
+  end
+
+  create_table "doctors", force: :cascade do |t|
+    t.string "name"
+    t.bigint "category_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_doctors_on_category_id"
+    t.index ["user_id"], name: "index_doctors_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "phone", default: "", null: false
@@ -27,4 +61,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_29_142125) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "appointments", "doctors"
+  add_foreign_key "appointments", "users"
+  add_foreign_key "doctors", "categories"
+  add_foreign_key "doctors", "users"
 end
