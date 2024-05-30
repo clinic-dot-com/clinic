2ab705daa92f6b2e5c8bf5a313dd8af39d5a1501
Anton-Ivanov <anton.i@hey.com>
2024-05-30 07:01:05 +0300

    add system test to cover changes about build info

--- spec/system/root_system_spec.rb
+++ spec/system/root_system_spec.rb
@@ -12,5 +12,10 @@ RSpec.describe 'Root path' do
       expect(page).to have_selector 'h1', exact_text: 'Home#index'
       expect(page).to have_current_path root_path
     end
+
+    it 'should display additional info about build' do
+      expect(page).to have_selector '.build-info', text: 'Build information: version:'
+      expect(page).to have_link 'Status page', href: 'https://clinic.betteruptime.com'
+    end
   end
 end
