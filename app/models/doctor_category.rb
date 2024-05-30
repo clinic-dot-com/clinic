cedbe6e5bde506c2e77ac89d9f21e639e81b5550
Ivanov-Anton <anton.i@hey.com>
2024-05-30 18:45:03 +0300

    add DoctorCategory ActiveAdmin resource (#36)

--- app/models/doctor_category.rb
+++ app/models/doctor_category.rb
@@ -15,4 +15,8 @@
 #
 class DoctorCategory < ApplicationRecord
   validates :name, uniqueness: true
+
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[created_at id id_value name updated_at]
+  end
 end
