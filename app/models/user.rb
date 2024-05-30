80aa6cf9415ccf0eda60d73db666c6a4608b4f5c
Ivanov-Anton <anton.i@hey.com>
2024-05-30 09:35:56 +0300

    add annotate gem & annotate existed model & add workflow (#23)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,24 @@
 # frozen_string_literal: true
 
-##
-# The primary model used for authentication
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  encrypted_password     :string           default(""), not null
+#  phone                  :string           default(""), not null
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  role                   :string           not null
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_phone                 (phone) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
