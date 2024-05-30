8486974d66ac39d90ac68bca1d0ef4f1cdae85c1
Anton-Ivanov <anton.i@hey.com>
2024-05-30 15:37:12 +0300

    introduce the Appointment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_30_105949) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_30_122103) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "appointments", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "status"
+    t.text "recommendation"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_appointments_on_user_id"
+  end
 
   create_table "doctor_categories", force: :cascade do |t|
     t.string "name"
@@ -33,4 +42,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_30_105949) do
     t.index ["phone"], name: "index_users_on_phone", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "appointments", "users"
 end
