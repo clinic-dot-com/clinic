9a91c42b4df7d0bab0256fba678366251a705eae
Ivanov-Anton <anton.i@hey.com>
2024-05-30 18:54:44 +0300

    add Appointment ActiveAdmin resource (#38)

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -21,4 +21,12 @@
 #
 class Appointment < ApplicationRecord
   belongs_to :user
+
+  def self.ransackable_associations(_auth_object = nil)
+    ['user']
+  end
+
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[created_at id id_value recommendation status updated_at user_id]
+  end
 end
