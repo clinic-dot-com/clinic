65af56cb643f95682cb39640441861bc6b2bb3cf
Ivanov-Anton <anton.i@hey.com>
2024-05-30 07:16:17 +0300

    Add build info (#19)
    - add build version to determinate what version is currently deployed

--- spec/system/sign_in_system_spec.rb
+++ spec/system/sign_in_system_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe 'Sign In' do
       expect(page).to have_button 'Log in'
       expect(page).to have_link 'Sign up', href: new_user_registration_path
       expect(page).to have_link 'Forgot your password?', href: new_user_password_path
-      expect(page).to have_link count: 2
+      expect(page).to have_link count: 3
       expect(page).to have_field count: 3
       expect(page).to have_current_path new_user_session_path
     end
