ee2979ea954d1df53f72d34a3e99ed4ba8ff8daa
Ivanov-Anton <anton.i@hey.com>
2024-05-31 19:48:18 +0300

    improve the root page of web application (#58)

--- spec/system/root_system_spec.rb
+++ spec/system/root_system_spec.rb
@@ -9,7 +9,8 @@ RSpec.describe 'Root path' do
     before { visit '/' }
 
     it 'should render home controller' do
-      expect(page).to have_selector 'h1', exact_text: 'Home#index'
+      expect(page).to have_selector 'h1', exact_text: 'Welcome to clinic management system'
+      expect(page).to have_selector 'h3', exact_text: 'the most popular clinic in Ukrainer'
       expect(page).to have_current_path root_path
     end
   end
